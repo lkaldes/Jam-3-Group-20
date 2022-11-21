88252a1ca3bbf174da9d7f28d1bd6433ea8d93ac
Aaron Gonzales <agonz243@ucsc.edu>
2022-11-20 19:17:50 -0800

    Added player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get orientation
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.right * x + transform.forward * z;
+
+        controller.Move(movement * speed * Time.deltaTime);
+    }
+}
