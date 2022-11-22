6309c6126b5f722e4cb92fe865bd214486ee48d4
Matthew <mchen146@ucsc.edu>
2022-11-21 18:44:53 -0800

    setting up inventory methods

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#nullable enable
 public class Inventory : MonoBehaviour
 {
+    public List<Item> items = new();
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,43 @@ public class Inventory : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public Item? ItemAt(int index)
+    {
+        if (index >= 0 && index < items.Count)
+        {
+            return items[index];
+        }
+        return null;
+    }
+
+    public bool HasItem(Item item)
+    {
+        return items.Contains(item);
+    }
+
+    public void AddItem(Item item)
+    {
+        if (!HasItem(item))
+        {
+            items.Add(item);
+        }
+    }
+
+    public void RemoveItem(Item item)
+    {
+        if (HasItem(item)) 
+        {
+            items.Remove(item);
+        }
+    }
+
+    public void RemoveItemAt(int index)
+    {
+        if (index >= 0 && index < items.Count)
+        {
+            items.RemoveAt(index);
+        }
     }
 }
